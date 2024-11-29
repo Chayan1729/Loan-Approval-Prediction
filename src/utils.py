2773a9b04098d1222028ecb508f87179280bfb9a
Chayan Halder <143322211+Chayan1729@users.noreply.github.com>
2024-11-29 19:28:33 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -4,15 +4,19 @@ import pickle
 from imblearn.over_sampling import SMOTE
 from imblearn.pipeline import Pipeline as ImbPipeline
 import os, sys
+import joblib
+# file_path="E:/Project Loan Prediction/Loan-Approval-Prediction/notebook/pickle_file/GradientBoosting_best_model.pkl"
 
 # To load the pickle file
 def load_object(file_path):
     try:
         logging.info(f"Loading pickle file from path: {file_path}")
-        with open(file_path, 'rb') as file_obj:
-            obj = pickle.load(file_obj)
-            logging.info("Pickle file loaded successfully")
-            return obj
+        model = joblib.load(file_path)
+        logging.info("Pickle file loaded successfully")
+        return model
+    except ModuleNotFoundError as e:
+        logging.error(f"ModuleNotFoundError: {e}")
+        raise CustomException(f"Module not found while loading pickle: {e}", sys)
     except Exception as e:
         logging.exception("Error loading pickle file")
-        raise CustomException(e, sys)
+        raise CustomException(f"Error loading pickle file: {e}", sys)
